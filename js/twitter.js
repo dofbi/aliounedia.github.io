8b43ada1dfafe217d87efd4e3f4e73b6245ae964
aliounedia <dia.aliounes@gmail.com>
2013-08-21 13:53:48 +0200

    update

--- js/twitter.js
+++ js/twitter.js
@@ -3,15 +3,15 @@
 //http://cypressnorth.com/social-media/how-to-easily-display-a-twitter-feed-using-javascript/
 
 Twitter =  function(){
-	var self = this ;
-	var _url = 'https://api.twitter.com/1/statuses/user_timeline/CypressNorth.json?callback=?&count=1';
+    var self = this ;
+    var _url = 'https://api.twitter.com/1/statuses/user_timeline/CypressNorth.json?callback=?&count=1';
 }
 
 // Twitter Parsing URL 
 Twitter.protocole.parse_url = function(){
     return this.replace(
 	/[A-Za-z]+://[A-Za-z0-9-_]+.[A-Za-z0-9-_:%&~?/.=]+/g, 
-	function(url) {
+        function(url) {
         return url.link(url);
     });
 };
@@ -40,44 +40,42 @@ Twitter.prototype.parse_date = function(str) {
 // Fetch Tweets
 Twitter.prototype.fetch  = function(){
     $.getJSON(self._url,function(data){
-	  for( i =0 ; i <data.length; i++){
-        	var tweet = data[0].text;
-        	var tweet_date = self.parseDate(data[0].created_at)
-		return 
-	  }
+        for( i =0 ; i <data.length; i++){
+            var tweet = data[0].text;
+            var tweet_date = self.parseDate(data[0].created_at)
+            return
+        }
     });
 }
 
 // Single Tweeter View , 
 TwitterView = new BackBone.Model.View(
-	initialize :function(){
-		//
-	}
-	template : '<div class="tweeter-info"><div class="uppercase bold"> \
-		<a href="https://twitter.com/#!/CypressNorth" target="_blank" class="black"> \
-		@CypressNorth</a></div><div class="right"> \
-		<a href="https://twitter.com/#!/CypressNorth/status/ \
-		'+data[0].id_str+'">'+tweet_date+' </a></div></div> \
- 	'
-	render : function(){
-		return Mustache.render(this.template, self.model);
- 	}
+    initialize :function(){
+        //
+    }
+    template : '<div class="tweeter-info"><div class="uppercase bold"> \
+    <a href="https://twitter.com/#!/CypressNorth" target="_blank" class="black"> \
+    @CypressNorth</a></div><div class="right"> \
+    <a href="https://twitter.com/#!/CypressNorth/status/ \
+    '+data[0].id_str+'">'+tweet_date+' </a></div></div> \
+    '
+    render : function(){
+        return Mustache.render(this.template, self.model);
+    }
  	
 }
-
 // Collections of tweets , render each tweet view
 TwittersView = new BackBone.Model.View(
-	initialize :function(){
-		tweets =  new Twitter().fetch()
-	}
- 	render : function(){
-		_.each( tweets  , function(tweets){
-		this.$el.append(new TwitterView({model: tweets}.render()))
-		return this
- 	}
+    initialize :function(){
+        tweets =  new Twitter().fetch()
+    }
+    render : function(){
+        _.each( tweets  , function(tweets){
+        this.$el.append(new TwitterView({model: tweets}.render()))
+        return this
+    }
 }
-
 // Setup Tweet 
 $(function(){
-	new  TwittersView()
+    new  TwittersView()
 })
--- js/vimeo.js
+++ js/vimeo.js
@@ -1,86 +1,88 @@
-//Vimeo  Api  . Vimeo Api , by Alioune Dia, Inspired by 
+//Vimeo Api. Vimeo Api, by Alioune Dia, Inspired by 
 //Andrew Nesbitt
-
-Vimeo       = function(){
-	var self    = this 
-	this.videos = [];
-	//this.keyword; // Keyword to filter videos by.
-	//this.user = 'user20281826'
-	//this.user = 'user202823'
-	//this.user   = 'forwardtechnology'
-	//this.type   = 'videos';
+Vimeo = function(){
+    var self    = this 
+    this.videos = [];
+    //this.keyword; // Keyword to filter videos by.
+    //this.user = 'user20281826'
+    //this.user = 'user202823'
+    //this.user = 'forwardtechnology'
+    //this.type = 'videos';
 
 }
-
 //Format  Video
 Vimeo.prototype.load =  function(data){
-	for( i = 0; i< data.length; i++) {
-		this.videos.push(
-		{
-		 title : data[i].title ,
-		 url   : data[i].url ,
-		 thumbnail_medium : data[i].thumbnail_medium 
-		});
-	}
-		
+    for( i = 0; i< data.length; i++) {
+        this.videos.push({
+        title : data[i].title ,
+        url   : data[i].url ,
+        thumbnail_medium : data[i].thumbnail_medium 
+        });
+    }		
 }
-
+/*jshint multistr:true */
 //render a item
 VimeoView =  Backbone.View.extend({
-	initialize : function(){
-		this.render();
-		return this
-	},
-	render : function(){
-		return Mustache.render(
-			"<li>{{title}}<li>", this.model);
-	}
+    template: ' <li class="video">\
+<a href="{{ url }}" class="link" target="_blank">\
+<img class="thumb" src="{{ thumbnail_medium }}" \
+alt="LNUG Jan 2013 - Darach Ennis" width="150">\
+<p class="title">{{ title }}</p>\
+</a>\
+</li>\
+    ',
+    initialize : function(){
+        this.render();
+    },
+    render : function(){
+        return Mustache.render(
+            this.template, this.model);
+    }
 }
 )
-
 //render a collection 
 VimeosView =  Backbone.View.extend({
-	el: $("#vimeo"),
-	initialize : function(){
-		this.render();
-		return this
-	},
-	render : function(){
-		var v ="";
-		_.each(this.model , 
-			function(video){
-			//test
-		alert("video" + video.url);
-		v   = v + new VimeoView({model : video}).render().$el
-			
-			}
-		)
-		this.$el.append(v);
-	}
+    el: ("#vimeo ul"),
+    initialize : function(){
+        this.render();
+        },
+        render : function(){
+            var v ="";
+            _.each(this.model , 
+            function(video){
+                //test
+                //alert("video" + video.url);
+                v = v + new VimeoView(
+                    {model : video}
+                ).render()	
+            }
+	    )
+	    this.$el.append(v);
+            return this
+        }
 }
 )
-
 //Initializing videos
 my_init = function(){
-  	var options;
-  	options = {
-    		url :"http://vimeo.com/api/v2/forwardtechnology/videos.json",
-		port: 80,
-  	}
-	$.ajax({
-              url: options.url,
-              dataType: "jsonp",
-              crossDomain: true
-		 
-            }
-	).done(function(data){
-		 v = new Vimeo()
-		 v.load(data);
-		 new  VimeosView({model: v.videos});
-	}
-	)
+    var options;
+    options = {
+        url :"http://vimeo.com/api/v2/\
+forwardtechnology/videos.json",
+        port: 80,
+    }
+    $.ajax({
+        url: options.url,
+        dataType: "jsonp",
+        crossDomain: true
+    }
+    ).done(function(data){
+        v = new Vimeo()
+        v.load(data);
+        alert("test");
+        new  VimeosView({model: v.videos});
+    }
+)
 }
-
 $(function(){
-	my_init();
+    my_init();
 })
